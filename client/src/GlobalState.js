f7dfa4f52ab039545da0b6accd349262c4824ea6
Tan <trongtandoan@gmail.com>
2023-07-11 17:10:44 +0700

    update 510

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,6 +1,27 @@
-import React from "react";
-import { createContext, useState, useEffect } from "react";
+// import React from "react";
+// import { createContext, useState, useEffect } from "react";
 
+// import io from "socket.io-client";
+
+// export const DataContext = createContext();
+
+// export const DataProvider = ({ children }) => {
+//   const [socket, setSocket] = useState(null);
+
+//   useEffect(() => {
+//     const socket = io();
+//     setSocket(socket);
+//     return () => socket.close();
+//   }, []);
+
+//   const state = {
+//     socket,
+//   };
+
+//   return <DataContext.Provider value={state}>{children}</DataContext.Provider>;
+// };
+
+import React, { createContext, useState, useEffect } from "react";
 import io from "socket.io-client";
 
 export const DataContext = createContext();
@@ -9,14 +30,29 @@ export const DataProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socket = io();
-    setSocket(socket);
-    return () => socket.close();
+    const connectSocket = () => {
+      const socketInstance = io("https://ezlish-server.onrender.com");
+
+      socketInstance.on("connect", () => {
+        console.log("Socket connected");
+      });
+
+      socketInstance.on("disconnect", () => {
+        console.log("Socket disconnected");
+      });
+
+      setSocket(socketInstance);
+    };
+
+    connectSocket();
+
+    return () => {
+      if (socket) {
+        socket.disconnect();
+        console.log("Socket disconnected");
+      }
+    };
   }, []);
 
-  const state = {
-    socket,
-  };
-
-  return <DataContext.Provider value={state}>{children}</DataContext.Provider>;
+  return <DataContext.Provider value={{ socket }}>{children}</DataContext.Provider>;
 };
